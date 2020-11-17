f144feba7aa935a8582f36fdc6eaa2f5a397a759
Vinh Le <lethanhvinh95@gmail.com>
2020-11-17 07:25:56 +0200

    refactor: create base recipe viewset class

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,12 +2,10 @@ from rest_framework import viewsets, mixins, authentication, permissions
 from . import serializers
 from core.models import Tag, Ingredient
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-	"""Manage all tags in the db"""
-	serializer_class = serializers.TagSerializer
+class BaseRecipeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+	"""Base configurations for Recipe attributes (Tags, Ingredients,...)"""
 	authentication_classes = (authentication.TokenAuthentication,)
 	permission_classes = (permissions.IsAuthenticated,)
-	queryset = Tag.objects.all()
 
 	# Overwrite default method
 	def get_queryset(self):
@@ -18,18 +16,13 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
 		"""Create a new tag for an user"""
 		serializer.save(user=self.request.user)
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeViewSet):
+	"""Manage all tags in the db"""
+	serializer_class = serializers.TagSerializer
+	queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeViewSet):
 	"""Manage all ingredients in the db"""
 	serializer_class = serializers.IngredientSerializer
-	authentication_classes = (authentication.TokenAuthentication,)
-	permission_classes = (permissions.IsAuthenticated,)
 	queryset = Ingredient.objects.all()
-
-	# Overwrite default method
-	def get_queryset(self):
-		"""Return only ingredients belong to current authenticated user"""
-		return self.queryset.filter(user=self.request.user).order_by('name')
-
-	def perform_create(self, serializer):
-		"""Create a new tag for an user"""
-		serializer.save(user=self.request.user)
