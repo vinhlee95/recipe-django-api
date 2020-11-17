44019be225f37b268670fb8dc42f7e0e45aaaaf9
Vinh Le <lethanhvinh95@gmail.com>
2020-11-17 07:12:07 +0200

    refactor: remove trailing slash in urls

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from . import views
 
 app_name = 'recipe'
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register('tags', views.TagViewSet)
-router.register('ingredient', views.IngredientViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 urlpatterns = [
 	path('', include(router.urls))
